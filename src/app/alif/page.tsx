907a2c286b9914a710d6770527d970934e17c824
Alif Rezky <teknoalif@mailo.com>
2026-08-05 00:37:22 +0800

    Delete src/app/alif directory

--- src/app/alif/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function AlifPage() {
-    return (
-        <div className="min-h-screen flex items-center justify-center bg-slate-50">
-        <h1 className="text-3xl font-bold">Profil Founder</h1>
-        </div>
-    );
-}
